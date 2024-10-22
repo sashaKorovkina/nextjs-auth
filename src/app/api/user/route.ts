d4185820f61aa2b0704595fbd36ce5f241b87b61
sasha <sasha@cmi2i.com>
2024-10-22 11:41:53 +0100

    added a simple api route

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server"
+import { db } from "@/lib/db"
+
+export async function POST(req: Request) { 
+    try {
+        const body = await req.json()
+        const { email, username, password } = body
+
+        // check if email exists 
+        const existingUserByEmail = await db.user.findUnique({
+            where: { email: email }
+        });
+
+        if(existingUserByEmail) {
+            return NextResponse.json({ user: null,  message: "Email already exists" }, { status: 409 })
+        }
+
+        // check if username exists 
+        const existingUserByUsername = await db.user.findUnique({
+            where: { username: username }
+        });
+        if(existingUserByUsername) {
+            return NextResponse.json({ user: null,  message: "Username already exists" }, { status: 409 })
+        }
+
+        const newUser = await db.user.create({
+            data: {
+                email,
+                username,
+                password
+            }
+        })
+
+        return NextResponse.json({ body })
+    } catch (error) {
+        
+    }
+} 
