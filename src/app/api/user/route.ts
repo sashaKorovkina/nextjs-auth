c9d92ee3920303f3bbb786993f188b823d0240e0
sasha <sasha@cmi2i.com>
2024-10-22 11:47:42 +0100

    api taken out

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,38 +1,38 @@
-import { NextResponse } from "next/server"
-import { db } from "@/lib/db"
+// import { NextResponse } from "next/server"
+// import { db } from "@/lib/db"
 
-export async function POST(req: Request) { 
-    try {
-        const body = await req.json()
-        const { email, username, password } = body
+// export async function POST(req: Request) { 
+//     try {
+//         const body = await req.json()
+//         const { email, username, password } = body
 
-        // check if email exists 
-        const existingUserByEmail = await db.user.findUnique({
-            where: { email: email }
-        });
+//         // check if email exists 
+//         const existingUserByEmail = await db.user.findUnique({
+//             where: { email: email }
+//         });
 
-        if(existingUserByEmail) {
-            return NextResponse.json({ user: null,  message: "Email already exists" }, { status: 409 })
-        }
+//         if(existingUserByEmail) {
+//             return NextResponse.json({ user: null,  message: "Email already exists" }, { status: 409 })
+//         }
 
-        // check if username exists 
-        const existingUserByUsername = await db.user.findUnique({
-            where: { username: username }
-        });
-        if(existingUserByUsername) {
-            return NextResponse.json({ user: null,  message: "Username already exists" }, { status: 409 })
-        }
+//         // check if username exists 
+//         const existingUserByUsername = await db.user.findUnique({
+//             where: { username: username }
+//         });
+//         if(existingUserByUsername) {
+//             return NextResponse.json({ user: null,  message: "Username already exists" }, { status: 409 })
+//         }
 
-        const newUser = await db.user.create({
-            data: {
-                email,
-                username,
-                password
-            }
-        })
+//         const newUser = await db.user.create({
+//             data: {
+//                 email,
+//                 username,
+//                 password
+//             }
+//         })
 
-        return NextResponse.json({ body })
-    } catch (error) {
+//         return NextResponse.json({ body })
+//     } catch (error) {
         
-    }
-} 
+//     }
+// } 
